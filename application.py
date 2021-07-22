451c96542e748fc23748eaebc6138bc2ae75fccf
mikaleroy <84069751+mikaleroy@users.noreply.github.com>
2021-07-22 13:44:03 +0200

    Update application.py

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ def gauge(labels=['LOW','HIGH'], colors='jet_r', arrow=1, title=''):
     # set the bottom banner and the title
     r = Rectangle((-0.4,-0.1),0.8,0.1, facecolor='w', lw=2)
     ax.add_patch(r)
-    ax.text(0, -0.5, title, horizontalalignment='center', \
+    ax.text(0, -0.09, title, horizontalalignment='center', \
          verticalalignment='center', fontsize=70, fontweight='bold')
 
     # plots the arrow now
@@ -241,7 +241,7 @@ if __name__ == "__main__":
             st.pyplot(gauge(labels=['Granted', 'Rejected'] ,
                           colors=[yes_color, no_color],
                           arrow=180-predicted_probas.loc[id_sk].proba*100*1.8-(50-threshold*100)*1.8,
-                          title='Score \n {:.2%}'.format(predicted_probas.loc[id_sk].proba)
+                          title='\n {:.2%}'.format(predicted_probas.loc[id_sk].proba)
                           )
                    )
 
